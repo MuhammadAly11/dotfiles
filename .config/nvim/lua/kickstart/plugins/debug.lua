35aba0a8eafd20bd5d3387d1f61ca9a396e4e637
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2024-08-04 10:08:39 +0300

    working cpp debugger

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -22,7 +22,7 @@ return {
 		"jay-babu/mason-nvim-dap.nvim",
 
 		-- Add your own debuggers here
-		"leoluz/nvim-dap-go",
+		-- "leoluz/nvim-dap-go",
 	},
 	keys = function(_, keys)
 		local dap = require("dap")
@@ -63,7 +63,8 @@ return {
 			-- online, please don't ask me how to install them :)
 			ensure_installed = {
 				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
+				-- "delve",
+				"codelldb",
 			},
 		})
 
@@ -155,38 +156,47 @@ return {
 		dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
 		-- Install golang specific config
-		require("dap-go").setup({
-			delve = {
-				-- On Windows delve must be run attached or it crashes.
-				-- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-				detached = vim.fn.has("win32") == 0,
+		-- require("dap-go").setup({
+		-- 	delve = {
+		-- 		-- On Windows delve must be run attached or it crashes.
+		-- 		-- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+		-- 		detached = vim.fn.has("win32") == 0,
+		-- 	},
+		-- })
+
+		-- dap.adapters.gdb = {
+		-- 	type = "executable",
+		-- 	command = "/usr/bin/gdb",
+		-- 	args = { "-i", "dap" },
+		-- }
+		--
+		-- dap.configurations.cpp = {
+		-- 	{
+		-- 		name = "Launch CPP",
+		-- 		type = "gdb",
+		-- 		request = "launch",
+		-- 		program = function()
+		-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		-- 		end,
+		-- 		cwd = "${workspaceFolder}",
+		-- 		stopAtBeginningOfMainSubprogram = false,
+		-- 	},
+		-- }
+		--
+		-- dap.defaults.fallback.focus_terminal = true
+		--
+		-- dap.defaults.gdb.external_terminal = {
+		-- 	command = "/usr/bin/alacritty",
+		-- 	args = { "-e" },
+		-- }
+		--
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				command = "codelldb",
+				args = { "--port", "${port}" },
 			},
-		})
-
-		dap.adapters.gdb = {
-			type = "executable",
-			command = "/usr/bin/gdb",
-			args = { "-i", "dap" },
-		}
-
-		dap.configurations.cpp = {
-			{
-				name = "Launch CPP",
-				type = "gdb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-				stopAtBeginningOfMainSubprogram = false,
-			},
-		}
-
-		dap.defaults.fallback.focus_terminal = true
-
-		dap.defaults.gdb.external_terminal = {
-			command = "/usr/bin/alacritty",
-			args = { "-e" },
 		}
 	end,
 }
