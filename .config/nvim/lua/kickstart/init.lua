770ca4e09ce3ee4a9b90eb9bb1f0a7613b8aa071
Mohammad Aly <176561401+MuhammadAly11@users.noreply.github.com>
2024-08-27 00:48:30 +0300

    nvim: added bash lsp

--- .config/nvim/lua/kickstart/init.lua
+++ .config/nvim/lua/kickstart/init.lua
@@ -577,6 +577,8 @@ require("lazy").setup({
 				clangd = {},
 				-- gopls = {},
 				pyright = {},
+				bashls = {},
+				beautysh = {},
 				typst_lsp = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
